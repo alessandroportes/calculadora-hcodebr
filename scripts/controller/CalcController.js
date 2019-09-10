bf6b1b30ec8f58ca7a0bd574cb4e15ac60ab9a92
Alessandro Marques Portes <amportes@REDE.local>
2019-09-10 16:09:47 -0300

    switch/case botões

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -16,12 +16,75 @@ class CalcController
         this.displayCalc = "0";
     }
 
+    execBtn(value){
+        switch (value) {
+            case 'CE':
+                console.log('CE');
+                break;
+            case 'C':
+                console.log('C');
+                break;                
+            case '+':
+                console.log('ADIÇÃO');
+                break;
+            case '-':
+                console.log('SUBTRAÇÃO');
+                break;
+            case '÷':
+                console.log('DIVISÃO');
+                break;                                
+            case 'X':
+                console.log('MULTIPLICAÇÃO');
+                break;
+            case '=':
+                console.log('IGUAL');
+                break;
+            case ',':
+                console.log('PONTO');
+                break;
+            case '±':
+                console.log('MAIS/MENOS');
+                break;
+            case '←':
+                console.log('BACKSPACE');
+                break;                
+            case '%':
+                console.log('PORCENTAGEM');
+                break;                
+            case '√':
+                console.log('RAIZ QUADRADA');
+                break;
+            case 'x²':
+                console.log('POTENCIA');
+                break;
+            case '¹/x':
+                console.log('1/X');
+                break;                
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                console.log('NUMEROS');
+                break;                                
+            default:
+                this.displayCalc = "ERROR";
+                break;
+        }
+    }
+
     initButtonEvents(){
         let buttons = document.querySelectorAll(".btn");
 
         buttons.forEach((btn, index) => {
             addEventListenerAll(btn, "click drag", e=>{
-                console.log(btn.innerHTML);
+                console.log('Botão', btn.innerHTML);
+                this.execBtn(btn.innerHTML);
             });
  
          });
